247f1fac15d691f5f971c0ad5d145bee57d599a8
WAN719 <3425446714@qq.com>
2026-05-06 10:52:33 +0800

    初始化系统结构

---
+++ backend/intern-pilot-backend/src/main/java/com/internpilot/InternPilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternPilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternPilotBackendApplication.class, args);
+	}
+
+}
---
+++ backend/intern-pilot-backend/src/test/java/com/internpilot/InternPilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternPilotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
